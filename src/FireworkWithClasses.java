f3d2a52fe82ffdb65ce61c1c98057fede4736724
Alexander Bazo <alexanderbazo@googlemail.com>
2021-11-23 10:17:25 +0100

    Add starter code

--- src/FireworkWithClasses.java
+++ src/FireworkWithClasses.java
@@ -2,7 +2,7 @@ import de.ur.mi.oop.app.GraphicsApp;
 import de.ur.mi.oop.colors.Color;
 import de.ur.mi.oop.launcher.GraphicsAppLauncher;
 
-public class Firework-With-Classes extends GraphicsApp {
+public class FireworkWithClasses extends GraphicsApp {
 
     private static final Color RED = new Color(234, 49, 63); // "Selbstgemischter" RGB-Farbe (rot)
     private static final Color YELLOW = new Color(234, 182, 56); // "Selbstgemischter" RGB-Farbe (gelb)
@@ -12,6 +12,7 @@ public class Firework-With-Classes extends GraphicsApp {
     private static final Color PURPLE = new Color(55, 39, 114); // "Selbstgemischter" RGB-Farbe (lila)
     private static final Color CREAM = new Color(241, 255, 250); // "Selbstgemischter" RGB-Farbe (creme)
     private static final Color GREY = new Color(47, 61, 76); // "Selbstgemischter" RGB-Farbe (grau)
+
     @Override
     public void initialize() {
         setCanvasSize(500, 500);
@@ -23,6 +24,6 @@ public class Firework-With-Classes extends GraphicsApp {
     }
 
     public static void main(String[] args) {
-        GraphicsAppLauncher.launch("Firework-With-Classes");
+        GraphicsAppLauncher.launch("FireworkWithClasses");
     }
 }
